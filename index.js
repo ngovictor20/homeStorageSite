9e25ba530c7fb45184f6a510f7b4eaaa9b0dc199
Victor <ngo.victor20@gmail.com>
2019-07-17 20:13:39 +0000

    added multer and bodyparser

---
+++ index.js
@@ -0,0 +1,34 @@
+const app = require('express')()
+const fs = require('fs');
+const multer = require('multer')
+const bodyParser = require('body-parser')
+var upload = multer();
+
+
+const hostname = '0.0.0.0';
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views',__dirname+'/views');
+app.set('view engine', 'ejs');
+
+
+app.get("/",(req,res)=>{
+	res.render("home");
+
+})
+
+app.post("/",upload.single('file'),(req,res) =>{
+	//handle file uploading
+	//console.log(req)
+	if(req.file){
+		console.log(req.file)
+		res.json(req.file)
+	}else{
+		console.log("no files uploaded")
+		res.send("post request for file upload, no file uploaded")
+	}
+})
+
+app.listen(port, hostname, ()=>{
+	console.log("Listening on port 3000")
+})
