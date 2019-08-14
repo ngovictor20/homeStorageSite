10b04615010d427c7ea867f09586d20fb70741b5
ngovictor20 <ngovictor20@users.noreply.github.com>
2019-08-13 21:56:03 -0400

    Adding folder deletion functionality

--- routes/folder.js
+++ routes/folder.js
@@ -7,77 +7,149 @@ const path = require("path");
 var upload = multer();
 const mongoose = require('mongoose')
 const Folder = require("../models/folder")
+const File = require("../models/files")
+
+
+router.get("/folder", (req, res) => {
+    res.redirect("/folder/5d52e45cbe36d951897979d4/")
+})
 
 
 //render
-router.get("/folder/:folder_id", (req,res)=>{
+router.get("/folder/:folder_id", (req, res) => {
     console.log("Folder Route")
-    Folder.findById(req.params.folder_id).populate("childFiles").populate("childFolders").populate("parentFolder").exec((err,doc)=>{
-        if(err){
+    Folder.findById(req.params.folder_id).populate("childFiles").populate("childFolders").populate("parentFolder").exec((err, doc) => {
+        if (err) {
             console.log(err)
             res.redirect("back")
-        }else{
+        } else {
             console.log("Hi")
             console.log(doc)
-            res.render("../views/folder/renderFolder",{folder : doc})
+            res.render("../views/folder/renderFolder", { folder: doc })
         }
     })
 })
 
 //create
-router.post("/folder/:folder_id/new",(req,res)=>{
+router.post("/folder/:folder_id/new", (req, res) => {
     console.log("creating folder document")
     console.log(req.body.folderName)
-    if(req.body.folderName){
-        Folder.findById(req.params.folder_id).populate("parentFolder").populate("childFolders").exec((err,folder)=>{
-            if(err){
+    if (req.body.folderName) {
+        Folder.findById(req.params.folder_id).populate("parentFolder").populate("childFolders").exec((err, folder) => {
+            if (err) {
                 console.log(err)
-                res.redirect("/folder/"+req.params.folder_id)
-            }else{
-                fs.mkdir(folder.path+req.body.folderName,(err)=>{
-                    if(err){
+                res.redirect("/folder/" + req.params.folder_id)
+            } else {
+                fs.mkdir(folder.path + req.body.folderName, (err) => {
+                    if (err) {
                         console.log("error with mkdir")
-                    }else{
+                    } else {
                         var newFolderObj = {
                             name: req.body.folderName,
-                            parentFolder:folder._id,
-                            path: folder.path+req.body.folderName+"\\"
+                            parentFolder: folder._id,
+                            path: folder.path + req.body.folderName + "\\"
                         }
-                        Folder.create(newFolderObj,(err,newFolder)=>{
-                            if(err){
+                        Folder.create(newFolderObj, (err, newFolder) => {
+                            if (err) {
                                 console.log(err)
-                            }else{
+                            } else {
                                 console.log(newFolder)
                                 folder.childFolders.push(newFolder._id)
                                 folder.save()
-                                res.redirect("/folder/"+newFolder._id)
+                                res.redirect("/folder/" + newFolder._id)
                             }
                         })
-                        
+
                     }
                 })
             }
-            
+
         })
     }
-    else{
+    else {
         console.log("Why am i here?")
         return
     }
 })
 
-
-//update
-//ajax
-router.post("folder/:folder_id/edit",(req,res)=>{
-    Folder.findByIdAndUpdate(req.params.folder_id,{},(err,doc)=>{
+//delete
+router.delete("/folder/:folder_id", (req, res) => {
+    console.log("Delete folder route")
+    // Folder.findByIdAndDelete(req.params.folder_id).populate("childFiles").exec((err, deletedDoc) => {
+    //     if (err) {
+    //         console.log(err)
+    //         console.log("ran into an error with finding")
+    //     } else {
+    //         deletedDoc.childFiles.forEach((x) => {
+    //             fs.unlink(x.path, (error) => {
+    //                 File.findByIdAndDelete(x._id, (delError, deletd) => {
+    //                     if (delError) {
+    //                         console.log(delError)
+    //                     } else {
+    //                         console.log("Deleted" + deletd.name + " from Mongo")
+    //                     }
+    //                 })
+    //             })
+    //         })
+    //         console.log("Done deleting folder")
+    //     }
+    // })
+    Folder.findByIdAndDelete(req.params.folder_id).populate("childFiles").exec((err,deletedDoc)=>{
+        let fun
         if(err){
             console.log(err)
+            console.log("ran into an error with finding folder")
         }else{
-            res.redirect("/folder/"+req.params.folder_id)
+            deletedDoc.childFiles.forEach((x)=>{
+                console.log("pushing")
+                fun.push(del(x))
+            })
+            Promise.all(fun).then(function(val){
+                console.log(val)
+            })
         }
     })
 })
+
+
+
+
+
+//update
+//ajax
+router.post("/folder/:folder_id/edit", (req, res) => {
+    Folder.findByIdAndUpdate(req.params.folder_id, {}, (err, doc) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect("/folder/" + req.params.folder_id)
+        }
+    })
+})
+
+
+function del(x) {
+    return new Promise(function (resolve, reject) {
+        fs.unlink(x.path, (error) => {
+            if (error) {
+                console.log("failed at unlink")
+                reject(error)
+            } else {
+                File.findByIdAndDelete(x._id, (delError, deletd) => {
+                    if (delError) {
+                        console.log(delError)
+                        reject("Failure: " + x._id)
+                    } else {
+                        console.log("Deleted" + deletd.name + " from Mongo")
+                        resolve(x._id)
+                    }
+                })
+            }
+        })
+    })
+}
+
+
 
 
 module.exports = router
--- routes/home.js
+++ routes/home.js
@@ -4,31 +4,9 @@ var multer = require("multer")
 var upload = multer();
 const fs = require("fs")
 
-function getDirContents() {
-	fs.readdir("./uploads", {
-		withFileTypes:true
-	}, (err, files) => {
-		if (err) {
-			throw (err)
-		} else {
-			let fileList = []
-			console.log("returning files")
-			console.log(files)
-			//res.render("displayDrive",{dir:files})
-			files.forEach(function (x) {
-				fil = path.parse(x)
-				fileList.push(
-					{
-						filename: fil.name,
-						ext: fil.ext,
-						stats: fs.statSync("./uploads/" + x)
-					}
-				)
-			})
-			console.log(fileList)
-			res.render("displayDrive", { dir: fileList })
-		}
-	})
-}
+
+router.get("/",(req,res)=>{
+	res.redirect("/folder/5d52e45cbe36d951897979d4/")
+})
 
 module.exports = router;
